161ef673e7d16ce1d88f74dd1fbf7e14f0a50a82
Dmytro Horban <dmytro.horban01@gmail.com>
2026-05-04 14:56:17 +0300

    Add: baseball-game - submission-2

---
+++ Data Structures & Algorithms/baseball-game/submission-2.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        List<Integer> stack = new ArrayList<>();
+        for (String op : operations) {
+            switch (op) {
+                case "D" -> {
+                    Integer number = stack.get(stack.size() - 1);
+                    Integer multiplication = 2 * number;
+                    stack.add(multiplication);
+                }
+                case "C" -> {
+                    stack.remove(stack.size() - 1);
+                }
+                case "+" -> {
+                    Integer secondNumber = stack.remove(stack.size() - 1);
+                    Integer firstNumber = stack.remove(stack.size() - 1);
+                    Integer sum = firstNumber + secondNumber;
+                    stack.add(firstNumber);
+                    stack.add(secondNumber);
+                    stack.add(sum);
+                }
+                default -> {
+                    stack.add(Integer.parseInt(op));
+                }
+            }
+        }
+
+        return stack.stream().mapToInt(Integer::intValue).sum();
+    }
+}
