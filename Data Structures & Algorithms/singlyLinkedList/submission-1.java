ee822ecfee8c5fecdd0a4cbfbf4ae84f862652de
Dmytro Horban <dmytro.horban01@gmail.com>
2026-05-05 16:03:55 +0300

    Add: singlyLinkedList - submission-1

---
+++ Data Structures & Algorithms/singlyLinkedList/submission-1.java
@@ -0,0 +1,93 @@
+class LinkedList {
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+
+    public LinkedList() {
+    }
+
+    public int get(int index) {
+        if (index >= size) 
+            return -1;
+        
+        int i = 0;
+        Node cur = head;
+        while (i != index) {
+            cur = cur.next;
+            i++;
+        }
+
+        return cur.value;
+    }
+
+    public void insertHead(int val) {
+        Node node = new Node();
+        node.value = val;
+        node.next = head;
+        head = node;
+        if (size == 0)
+            tail = node;
+
+        size++;
+    }
+
+    public void insertTail(int val) {
+        Node node = new Node();
+        node.value = val;
+        if (tail != null)
+            tail.next = node;
+
+        tail = node;
+
+        if (size == 0)
+            head = node;
+
+        size++;
+    }
+
+    public boolean remove(int index) {
+        if (index >= size || index < 0) 
+            return false;
+        
+        Node prevElement = null;
+        if (index == 0) {
+            head = head.next;
+        } else {
+            int findIndex = index - 1;
+            int i = 0;
+            prevElement = head;
+            while (i != findIndex) {
+                prevElement = prevElement.next;
+                i++;
+            }
+
+            prevElement.next = prevElement.next.next;
+        }
+
+        if (index == size - 1) {
+            tail = prevElement;
+        }
+
+        size--;
+
+        return true;
+    }
+
+    public ArrayList<Integer> getValues() {     
+        ArrayList<Integer> list = new ArrayList<>();
+        Node cur = head;
+        while (cur != null) {
+            list.add(cur.value);
+            cur = cur.next;
+        }
+
+        return list;
+    }
+
+    private static class Node {
+        private int value;
+        private Node next;
+    }
+}
