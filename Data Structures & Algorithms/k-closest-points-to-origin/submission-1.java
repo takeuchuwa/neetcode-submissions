c9dc5c1550876820615dc7f6fd3d78b11157b014
Dmytro Horban <dmytro.horban01@gmail.com>
2026-05-09 14:49:16 +0300

    Add: k-closest-points-to-origin - submission-1

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-1.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        quickSort(points, 0, points.length, k);
+        int[][] kClosest = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            kClosest[i] = points[i];
+        }
+
+        return kClosest;
+    }
+
+    public void quickSort(int[][] points, int start, int end, int k) {
+        if (end - start <= k) {
+            return;
+        }
+
+        int[] pivotXY = points[end - 1];
+        double pivot = Math.pow(pivotXY[0], 2) + Math.pow(pivotXY[1], 2);
+        int pointer = start;
+        for (int i = start; i < end; i++) {
+            double distance = Math.pow(points[i][0], 2) + Math.pow(points[i][1], 2);
+            if (distance < pivot) {
+                int[] temp = points[pointer];
+                points[pointer] = points[i];
+                points[i] = temp;
+                pointer++;
+            }
+        }
+
+        points[end - 1] = points[pointer];
+        points[pointer] = pivotXY;
+
+        
+        int countOfShortetsDistanceToTheLeft = pointer - start + 1;
+        if (countOfShortetsDistanceToTheLeft > k) {
+            quickSort(points, start, pointer, k);
+        } else {
+            quickSort(points, pointer + 1, end, k - countOfShortetsDistanceToTheLeft);
+        }
+        
+    }
+}
