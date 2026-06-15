bf3855c7e4a388340cf6063e30f5c0a73776d099
Dmytro Horban <dmytro.horban01@gmail.com>
2026-06-15 18:40:23 +0300

    Add: design-word-search-data-structure - submission-2

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-2.java
@@ -0,0 +1,49 @@
+class WordDictionary {
+
+    TrieNode root;
+
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+
+    public void addWord(String word) {
+        TrieNode curr = root;
+        for (Character c : word.toCharArray()) {
+            if (!curr.children.containsKey(c)) {
+                curr.children.put(c, new TrieNode());
+            }
+            curr = curr.children.get(c);
+        }
+        curr.word = true;
+    }
+
+    public boolean search(String word) {
+        return dfs(0, word, root);
+    }
+
+    private boolean dfs(int index, String word, TrieNode curr) {
+        for (int i = index; i < word.length(); i++) {
+            char c = word.charAt(i);
+
+            if (c == '.') {
+                for (TrieNode child : curr.children.values()) {
+                    if (dfs(i + 1, word, child)) {
+                        return true;
+                    }
+                }
+                return false;
+            } else {
+                if (!curr.children.containsKey(c)) {
+                    return false;
+                }
+                curr = curr.children.get(c);
+            }
+        }
+        return curr.word;
+    }
+
+    class TrieNode {
+        private Map<Character, TrieNode> children = new HashMap<>();
+        private boolean word;
+    }
+}
