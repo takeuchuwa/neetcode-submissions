38a7b00c25bf0263b8de702100d33be8c2a76e9b
Dmytro Horban <dmytro.horban01@gmail.com>
2026-07-22 18:10:46 +0300

    Add: target-sum - submission-4

---
+++ Data Structures & Algorithms/target-sum/submission-4.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        int n = nums.length;
+        int sum = 0;
+        for (int num: nums) {
+            sum += num;
+        }
+        int t = 2 * sum + 1;
+        int[] dp = new int[t];
+        dp[sum] = 1;
+
+        for (int num: nums) {
+            int[] nextDp = new int[t];
+            for (int i = 0; i < dp.length; i++) {
+                if (dp[i] != 0) {
+                    int total = i - sum;
+                    int count = dp[i];
+                    nextDp[i + num] += count;
+                    nextDp[i - num] += count;
+                }
+            }
+            dp = nextDp;
+        }   
+
+        int i = sum + target;
+        return i < t && i >= 0 ? dp[i] : 0;
+    }
+}
