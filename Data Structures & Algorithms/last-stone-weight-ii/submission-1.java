512099841fc07a20b6716a1dbda2874f505ad3be
Dmytro Horban <dmytro.horban01@gmail.com>
2026-07-23 20:25:39 +0300

    Add: last-stone-weight-ii - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight-ii/submission-1.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int lastStoneWeightII(int[] stones) {
+        int n = stones.length;
+        int sum = 0;
+        for (int stone: stones) {
+            sum += stone;
+        }
+        int target = sum / 2;
+        int[] dp = new int[target + 1];
+        int j = 0;
+        for (int stone: stones) {
+            int[] nextDp = new int[target + 1];
+            for (int i = 0; i <= target; i++) {
+                int skip = dp[i];
+                int val = 0;
+                if (i >= stone) {
+                    val = stone + dp[i - stone];
+                }
+                nextDp[i] = Math.max(val, skip);
+            }
+            System.out.println("dp: " + j++ + " " + Arrays.toString(nextDp));
+            dp = nextDp;
+        }
+
+        return sum - 2 * dp[target];
+    }
+}
