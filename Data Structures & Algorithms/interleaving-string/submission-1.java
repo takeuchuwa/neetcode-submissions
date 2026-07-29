a1ca7838d4aa2f880a0168017f62c03cafe1eadf
Dmytro Horban <dmytro.horban01@gmail.com>
2026-07-29 23:51:16 +0300

    Add: interleaving-string - submission-1

---
+++ Data Structures & Algorithms/interleaving-string/submission-1.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if (s3.length() != s1.length() + s2.length()) {
+            return false;
+        }
+        return dfsHelper(s1, s2, s3, 0, 0);
+    }
+
+    private boolean dfsHelper(String s1, String s2, String s3, int i1, int i2) {
+        int i3 = i1 + i2;
+        if (i1 == s1.length()) {
+            while (i2 != s2.length()) {
+                if (s2.charAt(i2) != s3.charAt(i3)) {
+                    return false;
+                }
+                i2++;
+                i3++;
+            }
+
+            return true;
+        } else if (i2 == s2.length()) {
+            while (i1 != s1.length()) {
+                if (s1.charAt(i1) != s3.charAt(i3)) {
+                    return false;
+                }
+                i1++;
+                i3++;
+            }
+
+            return true;
+        }
+        boolean res = false;
+        if (s1.charAt(i1) == s3.charAt(i3)) {
+            res |= dfsHelper(s1,s2,s3, i1 + 1, i2);
+        } 
+        if (s2.charAt(i2) == s3.charAt(i3)) {
+            res |= dfsHelper(s1,s2,s3, i1, i2 + 1);
+        }
+        
+        return res;
+    }
+}
