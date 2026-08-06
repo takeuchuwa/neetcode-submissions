e247f828e44da8df14792291e1ea5b3a91a41d69
Dmytro Horban <dmytro.horban01@gmail.com>
2026-08-06 23:23:05 +0300

    Add: zigzag-iterator - submission-3

---
+++ Data Structures & Algorithms/zigzag-iterator/submission-3.java
@@ -0,0 +1,42 @@
+class ZigzagIterator {
+
+    Queue<Iterator<Integer>> iterators;
+
+    public ZigzagIterator(List<Integer> v1, List<Integer> v2) {
+        iterators = new ArrayDeque<>();
+        Iterator<Integer> v1Iterator = v1.iterator();
+        if (v1Iterator.hasNext()) {
+            iterators.offer(v1Iterator);
+        }
+        Iterator<Integer> v2Iterator = v2.iterator();
+        if (v2Iterator.hasNext()) {
+            iterators.offer(v2Iterator);
+        }
+    }
+
+    public int next() {
+        Iterator<Integer> curr = iterators.poll();
+        int elem = curr.next();
+        if(curr.hasNext()) {
+            iterators.offer(curr);
+        }
+
+        return elem;
+    }
+
+    public boolean hasNext() {
+        for (Iterator<Integer> iterator: iterators) {
+            if (iterator.hasNext()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+/**
+ * Your ZigzagIterator object will be instantiated and called as such:
+ * ZigzagIterator i = new ZigzagIterator(v1, v2);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
