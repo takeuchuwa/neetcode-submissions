281a15a9c8a65b7d7e587ae680e5048b74710f26
Dmytro Horban <dmytro.horban01@gmail.com>
2026-08-07 16:23:41 +0300

    Add: dijkstra - submission-4

---
+++ Data Structures & Algorithms/dijkstra/submission-4.java
@@ -0,0 +1,43 @@
+class Solution {
+    public Map<Integer, Integer> shortestPath(int n, List<List<Integer>> edges, int src) {
+        List<List<NeighborNode>> adjList = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            adjList.add(new ArrayList<>());
+        }
+
+        for (List<Integer> edge: edges) {
+            adjList.get(edge.get(0)).add(new NeighborNode(edge.get(1), edge.get(2)));
+        }
+        Map<Integer, Integer> output = new HashMap<>();
+        Queue<NeighborNode> minHeap = new PriorityQueue<>((n1, n2) -> n1.weight - n2.weight);
+        minHeap.offer(new NeighborNode(src, 0));
+        while (!minHeap.isEmpty()) {
+            NeighborNode node = minHeap.poll();
+            if (output.containsKey(node.edge)) continue;
+
+            output.put(node.edge, node.weight);
+            for (NeighborNode neighbor: adjList.get(node.edge)) {
+                if (!output.containsKey(neighbor.edge)) {
+                    minHeap.offer(new NeighborNode(neighbor.edge, node.weight + neighbor.weight));
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            output.putIfAbsent(i, -1);
+        }
+
+        return output;
+    }
+
+    private static class NeighborNode {
+        int edge;
+        int weight;
+
+        NeighborNode(int edge, int weight) {
+            this.edge = edge;
+            this.weight = weight;
+        }
+    }  
+}
