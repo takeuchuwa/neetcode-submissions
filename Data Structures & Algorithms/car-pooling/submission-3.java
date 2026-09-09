124d2cd2fa1554885ba8ba3322292a4c11689748
Dmytro Horban <dmytro.horban01@gmail.com>
2026-09-09 13:26:52 +0300

    Add: car-pooling - submission-3

---
+++ Data Structures & Algorithms/car-pooling/submission-3.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        Arrays.sort(trips, Comparator.comparingInt(a -> a[1]));
+
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+        int curPass = 0;
+
+        for (int[] trip : trips) {
+            int numPass = trip[0], start = trip[1], end = trip[2];
+
+            while (!minHeap.isEmpty() && minHeap.peek()[0] <= start) {
+                curPass -= minHeap.poll()[1];
+            }
+
+            curPass += numPass;
+            if (curPass > capacity) {
+                return false;
+            }
+
+            minHeap.offer(new int[]{end, numPass});
+        }
+
+        return true;
+    }
+}
